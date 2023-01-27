f482ce24fbc223456c5dae42d924aa355cfeb731
Yeong joon <bluehome8626@naver.com>
2023-01-27 22:04:03 +0900

    ⚙️ :: 프로젝트 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
-import { SDSThemeProvider } from "@semicolondsm/react-emotion-theme";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { SDSThemeProvider } from "@semicolondsm/react-emotion-theme";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../styles/globalStyle";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SDSThemeProvider mode="light-only">
-      <Global styles={globalStyles} />
-      <Component {...pageProps} />
-    </SDSThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com/" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com/"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <SDSThemeProvider mode="light-only">
+        <Global styles={globalStyles} />
+        <Component {...pageProps} />
+      </SDSThemeProvider>
+    </>
   );
 }
