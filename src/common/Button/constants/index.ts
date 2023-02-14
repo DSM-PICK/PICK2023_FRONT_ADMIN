179831959956839c8d7debc08462d459e1fee83d
Kangyongsoo <aodtn2006@naver.com>
2023-02-14 17:45:32 +0900

    ⚡ :: (#8) 파일 나누기
    styleState를 constants폴더로 분리

---
+++ src/common/Button/constants/index.ts
@@ -0,0 +1,42 @@
+export const styleState = {
+  colorState: {
+    default: "#5C5961",
+    purple: "#F9F7FA",
+    red: "#F9F7FA",
+    ghost: "#98959E",
+  },
+  backColorState: {
+    default: "#F9F7FA",
+    purple: "#9650FA",
+    red: "#F04D51",
+    ghost: "#ffffff",
+  },
+  borderState: {
+    default: "0",
+    purple: "0",
+    red: "0",
+    ghost: "1px solid #EAE7EE",
+  },
+  hoverState: {
+    default: "#EAE7EE",
+    purple: "#8335F0",
+    red: "#D61A20",
+    ghost: "#F9F7FA",
+  },
+  disabledState: {
+    defautl: ``,
+    purple: `
+      background-color: #f0e6ff;
+      pointer-events: none;
+    `,
+    red: `
+      background-color: #ffe6e6;
+      pointer-events: none;
+    `,
+    ghost: `
+      border-color: #f2f0f5;
+      color: #dbd7e0;
+      pointer-events: none;
+    `,
+  },
+};
