560b4e8950bf874fd906969f70b7c82ee5d3af04
Kangyongsoo <aodtn2006@naver.com>
2023-02-14 17:45:32 +0900

    ⚡ :: 파일 나누기
    styleState를 constants폴더로 분리

--- src/common/Button/ButtonComponent.tsx
+++ src/common/Button/ButtonComponent.tsx
@@ -1,48 +1,6 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-
-const styleState = {
-  colorState: {
-    default: "#5C5961",
-    purple: "#F9F7FA",
-    red: "#F9F7FA",
-    ghost: "#98959E",
-  },
-  backColorState: {
-    default: "#F9F7FA",
-    purple: "#9650FA",
-    red: "#F04D51",
-    ghost: "#ffffff",
-  },
-  borderState: {
-    default: "0",
-    purple: "0",
-    red: "0",
-    ghost: "1px solid #EAE7EE",
-  },
-  hoverState: {
-    default: "#EAE7EE",
-    purple: "#8335F0",
-    red: "#D61A20",
-    ghost: "#F9F7FA",
-  },
-  disabledState: {
-    defautl: ``,
-    purple: `
-      background-color: #f0e6ff;
-      pointer-events: none;
-    `,
-    red: `
-      background-color: #ffe6e6;
-      pointer-events: none;
-    `,
-    ghost: `
-      border-color: #f2f0f5;
-      color: #dbd7e0;
-      pointer-events: none;
-    `,
-  },
-};
+import { styleState } from "./constants/index";
 
 interface BtnProps {
   children: string;
@@ -58,7 +16,13 @@ interface BtnStyle {
   [index: string]: string;
 }
 
-const ButtonComponent = ({ children, fill, disabled, onClick, customStyle }: BtnProps) => {
+const ButtonComponent = ({
+  children,
+  fill,
+  disabled,
+  onClick,
+  customStyle,
+}: BtnProps) => {
   interface BtnStyleVariable {
     [index: string]: BtnStyle;
   }
