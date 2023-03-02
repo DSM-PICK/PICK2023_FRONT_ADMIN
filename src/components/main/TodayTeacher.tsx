21cb3cc9318c834ea9e9d3e7c84cc9d09ff197ba
hyeyeonchurros <hyeyoenchurros@naver.com>
2023-03-02 11:33:31 +0900

    메인페이지 수정

---
+++ src/components/main/TodayTeacher.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  second_floor: string;
+  third_floor: string;
+  fourth_floor: string;
+}
+
+const TodayTeacher = ({ second_floor, third_floor, fourth_floor }: Props) => {
+  return (
+    <TodayTeacherContainer>
+      <p>오늘의 자습감독</p>
+      <div>
+        <p>2층 {second_floor}선생님</p>
+        <p>3층 {third_floor}선생님</p>
+        <p>4층 {fourth_floor}선생님</p>
+      </div>
+    </TodayTeacherContainer>
+  );
+};
+
+const TodayTeacherContainer = styled.div`
+  margin: 40px 0;
+  > p {
+    font-weight: 400;
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.gray600};
+  }
+  > div {
+    display: flex;
+    width: 100%;
+    height: 60px;
+    justify-content: space-evenly;
+    text-align: center;
+    border-radius: 16px;
+    background-color: ${({ theme }) => theme.colors.gray50};
+    margin-top: 20px;
+    > p {
+      font-size: 18px;
+      line-height: 60px;
+      color: ${({ theme }) => theme.colors.gray800};
+    }
+  }
+`;
+
+export default TodayTeacher;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,93 @@
-import Main from "@/components/main";
+import { Attendance, Outing, OutingList } from "@/assets/main";
+import Card from "@/components/main/Card";
+import Date from "@/components/main/Date";
+import TodayTeacher from "@/components/main/TodayTeacher";
+import styled from "@emotion/styled";
 
-
-export default function Home() {
-  return (
-    <>
-    <Main/>
-    </>
-  );
+interface Props {
+  picnic: number;
+  application: number;
+  classroom_movement: number;
 }
+
+const Home = ({ picnic, application, classroom_movement }: Props) => {
+  const OutingText = () => {
+    return (
+      <>
+        <p>현재 외출 신청 학생은</p>
+        <p>
+          총 <i>{picnic}명</i>입니다.
+        </p>
+      </>
+    );
+  };
+
+  const MoveText = () => {
+    return (
+      <>
+        <p>현재 2층에서 이동한</p>
+        <p>
+          학생 수는 <i>{application}명</i>입니다.
+        </p>
+      </>
+    );
+  };
+
+  const OutingListText = () => {
+    return (
+      <>
+        <p> 현재 외출 중인 학생은</p>
+        <p>
+          총 <i>{classroom_movement}명</i>입니다.
+        </p>
+      </>
+    );
+  };
+
+  return (
+    <MainPageContainer>
+      <Date date="2023-02-20" name="정대현" floor={[2, 3]} />
+      <TodayTeacher
+        second_floor="추혜연"
+        third_floor="신희원"
+        fourth_floor="정대현"
+      />
+      <CardContainer>
+        <Card
+          text={OutingText}
+          img={Outing}
+          btnText="외출 수락하러 가기"
+          link="/activity-accept"
+        />
+        <Card
+          text={MoveText}
+          img={Attendance}
+          btnText="출결 상태 확인하기"
+          link="/attendance"
+        />
+        <Card
+          text={OutingListText}
+          img={OutingList}
+          btnText="외출자 목록보기"
+          link="/outlist"
+        />
+      </CardContainer>
+    </MainPageContainer>
+  );
+};
+
+const MainPageContainer = styled.div`
+  width: 70vw;
+  max-width: 1220px;
+  margin: auto;
+  @media (max-width: 1100px) {
+    width: auto;
+  }
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default Home;
