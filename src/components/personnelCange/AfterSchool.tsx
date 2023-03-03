b71fcc70000e79d1e035a13349a36f7db1b201aa
김한비 <gksql2256@dsm.hs.kr>
2023-03-03 23:06:05 +0900

    ⚡ :: 인원변경(방과후) 페이지 에러처리

--- src/components/personnelCange/AfterSchool.tsx
+++ src/components/personnelCange/AfterSchool.tsx
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import ButtonComponent from "../common/button/ButtonComponent";
-<<<<<<< HEAD
 import { useState } from "react";
 import ModalPage from "../common/modal";
 
@@ -9,9 +8,6 @@ interface List {
   이름: string;
 }
 
-=======
-import { Colors } from "@semicolondsm/ui/dist/components/Button/types";
->>>>>>> #33-pick-admin-change-person-after-school-page
 const AfterSchool = () => {
   const StudentAdd = {
     "margin-top": "122px",
@@ -19,7 +15,6 @@ const AfterSchool = () => {
     "font-size": "16px",
     "line-height": "24px",
     "margin-left": "725px",
-<<<<<<< HEAD
     color: "#DBD7E0",
   };
 
@@ -114,139 +109,10 @@ const AfterSchool = () => {
     },
     {
       학번: "2218",
-=======
-   "color": "#DBD7E0",
-  };
-
-
-  return (
-    <>
-      <Wrapper>
-        <TitleContainer>
-          <Title>방과후 자습</Title>
-          <LayerText>2층 창조실</LayerText>
-          <ButtonComponent
-            fill="ghost"
-            size={["147px;", "48px"]}
-            customStyle={StudentAdd}
-          >
-            인원 추가하기
-          </ButtonComponent>
-        </TitleContainer>
-        <ListContainer>
-          <ListBox/>
-        </ListContainer>
-      </Wrapper>
-    </>
-  );
-};
-
-const ListBox = () => {
-  const Delete={
-    "margin-top":"12px",
-    "margin-left":"268px",
-    "font-weight": "500px",
-    "font-size": "16px",
-  }
-
-
-  const List = [
-    {
-      학번: 2120,
-      이름: "추혜연",
-    },
-    {
-      학번: 2419,
-      이름: "하혜령",
-    },
-    {
-      학번: 2106,
-      이름: "김의찬",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
-      이름: "정대현",
-    },
-    {
-      학번: 2218,
->>>>>>> #33-pick-admin-change-person-after-school-page
       이름: "정대현",
     },
   ];
 
-<<<<<<< HEAD
   return (
     <>
       <Wrapper>
@@ -279,15 +145,12 @@ const ListBox = ({ 학번, 이름 }: List) => {
     "font-size": "16px",
   };
 
-=======
->>>>>>> #33-pick-admin-change-person-after-school-page
   const StudentDelete = {
     "margin-top": "12px",
     "font-weight": "500px",
     "font-size": "16px",
     "line-height": "24px",
     "margin-left": "138px",
-<<<<<<< HEAD
     color: "#FFFFFF",
   };
 
@@ -324,25 +187,6 @@ const ListBox = ({ 학번, 이름 }: List) => {
   );
 };
 
-=======
-    "color":"#FFFFFF"
-  };
-  
-  return (
-    <>
-    {List.map((props) => (
-          <BoxWrapper>
-      <Student>{props.학번} {props.이름}</Student>
-        <ButtonComponent customStyle={StudentDelete} fill="purple" size={["72px", "36px"]}>
-          삭제
-        </ButtonComponent>
-      </BoxWrapper>
-          ))}
-      
-    </>
-  );
-};
->>>>>>> #33-pick-admin-change-person-after-school-page
 const Wrapper = styled.div`
   padding-left: 150px;
 `;
@@ -376,10 +220,6 @@ const ListContainer = styled.div`
   column-gap: 28px;
   display: grid;
   grid-template-columns: repeat(3, 360px);
-<<<<<<< HEAD
-=======
-  
->>>>>>> #33-pick-admin-change-person-after-school-page
 `;
 const BoxWrapper = styled.div`
   width: 360px;
@@ -387,7 +227,6 @@ const BoxWrapper = styled.div`
   background: #ffffff;
   border-radius: 12px;
   display: flex;
-<<<<<<< HEAD
 `;
 const Student = styled.div`
   font-weight: 500;
@@ -395,17 +234,6 @@ const Student = styled.div`
   line-height: 28px;
   margin-left: 24px;
   margin-top: 16px;
-=======
-
-  
-`;
-const Student = styled.div`
-font-weight: 500;
-font-size: 20px;
-line-height: 28px;
-margin-left: 24px;
-margin-top: 16px;
->>>>>>> #33-pick-admin-change-person-after-school-page
 `;
 
 export default AfterSchool;
