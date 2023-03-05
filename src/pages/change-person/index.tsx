f9352f0ea890854d11323a581bdab984a7bcb775
김한비 <gksql2256@dsm.hs.kr>
2023-03-05 23:03:13 +0900

    ⚡ :: 인원변경(방과후) 페이지 에러처리

--- src/pages/change-person/index.tsx
+++ src/pages/change-person/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 import AfterSchool from "@/components/personnelCange/AfterSchool";
-=======
-import AfterSchool from "@/components/personnelChange/AfterSchool";
->>>>>>> Stashed changes
 
 const changePerson = () => {
   return <AfterSchool />;
