fca0870950423ddba09f5de94ce77a4b53466af3
Yeong joon <bluehome8626@naver.com>
2023-03-06 15:16:30 +0900

    ⚡️:: (#46) login page에서 사이드바 안보이게하기 및 사이드바 로고 클릭시 main으로 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,105 +1,99 @@
-import { Attendance, Outing, OutingList } from "@/assets/main";
-import Card from "@/components/main/Card";
-import Date from "@/components/main/Date";
-import TodayTeacher from "@/components/main/TodayTeacher";
 import styled from "@emotion/styled";
-import { useQuery } from "react-query";
-import {
-  getTodaySelfStudyTeacher,
-  getTodaySelfStudyTeacherWhether,
-  getMainpageStudnetCount,
-} from "@/utils/api/selfStudy/index";
+import background from "../assets/login/background.png";
+import logo from "..//assets/login/logo.png";
+import Input from "../components/common/input/index";
+import { Button } from "@semicolondsm/ui";
+import Image from "next/image";
+import { useState, useLayoutEffect } from "react";
+import { NextPage } from "next";
 
-const Home = () => {
-  const { data } = useQuery("postlist", () => getTodaySelfStudyTeacher());
-  const { data: state } = useQuery("state", () =>
-    getTodaySelfStudyTeacherWhether()
-  );
-  const { data: students } = useQuery("students", () =>
-    getMainpageStudnetCount()
-  );
+const Home: NextPage = () => {
+  useLayoutEffect(() => {
+    const sideBar = document.getElementById("sidebar");
+    const spacer = document.getElementById("space");
+    sideBar && (sideBar.style.display = "none");
+    spacer && (spacer.style.display = "none");
+    return () => {
+      sideBar && (sideBar.style.display = "block");
+      spacer && (spacer.style.display = "block");
+    };
+  }, []);
 
-  const OutingText = () => {
-    return (
-      <>
-        <p>현재 외출 신청 학생은</p>
-        <p>
-          총 <i>{students?.data.picnic}명</i>입니다.
-        </p>
-      </>
-    );
-  };
-
-  const MoveText = () => {
-    return (
-      <>
-        <p>현재 2층에서 이동한</p>
-        <p>
-          학생 수는 <i>{students?.data.application}명</i>입니다.
-        </p>
-      </>
-    );
-  };
-
-  const OutingListText = () => {
-    return (
-      <>
-        <p> 현재 외출 중인 학생은</p>
-        <p>
-          총 <i>{students?.data.classroom_movement}명</i>입니다.
-        </p>
-      </>
-    );
-  };
+  const [loginData, setLoginData] = useState({
+    account_id: "",
+    password: "",
+  });
 
   return (
-    <MainPageContainer>
-      <Date
-        date={state?.data.date!}
-        name={state?.data.name!}
-        floor={state?.data.floor!}
-      />
-      <TodayTeacher
-        second_floor={data?.data.second_floor!}
-        third_floor={data?.data.third_floor!}
-        fourth_floor={data?.data.fourth_floor!}
-      />
-      <CardContainer>
-        <Card
-          text={OutingText}
-          img={Outing}
-          btnText="외출 수락하러 가기"
-          link="/activity-accept"
-        />
-        <Card
-          text={MoveText}
-          img={Attendance}
-          btnText="출결 상태 확인하기"
-          link="/attendance"
-        />
-        <Card
-          text={OutingListText}
-          img={OutingList}
-          btnText="외출자 목록보기"
-          link="/outlist"
-        />
-      </CardContainer>
-    </MainPageContainer>
+    <LoginContainer>
+      <LoginWrapper
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Image src={logo} width={50} height={50} alt="logo" />
+        <InputContainer>
+          <Input
+            placeholder="아이디를 입력하세요"
+            name="account_id"
+            value={loginData.account_id}
+            onChange={(e) =>
+              setLoginData((state) => ({
+                ...state,
+                [e.target.name]: e.target.value,
+              }))
+            }
+          />
+          <Input
+            placeholder="비밀번호를 입력하세요"
+            name="password"
+            value={loginData.password}
+            onChange={(e) =>
+              setLoginData((state) => ({
+                ...state,
+                [e.target.name]: e.target.value,
+              }))
+            }
+          />
+        </InputContainer>
+        <LoginButton fullWidth fill="purple">
+          로그인
+        </LoginButton>
+      </LoginWrapper>
+    </LoginContainer>
   );
 };
 
-const MainPageContainer = styled.div`
-  width: 70vw;
-  max-width: 1220px;
-  margin: auto;
-  @media (max-width: 1100px) {
-    width: auto;
-  }
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  background-image: url(${background.src});
+  background-size: cover;
 `;
 
-const CardContainer = styled.div`
+const LoginWrapper = styled.form`
+  padding: 50px 60px;
+  border-radius: 12px;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+  box-shadow: 0px 2px 8px rgba(33, 33, 33, 0.25);
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 300px;
+`;
+
+const LoginButton = styled(Button)`
+  border-radius: 12px;
 `;
 
 export default Home;
