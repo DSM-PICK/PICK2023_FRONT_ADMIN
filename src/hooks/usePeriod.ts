32ad646cf8dcbfe490a1f6ef938352bf50fcce0a
justinlee05 <justinlee05@naver.com>
2023-03-20 14:36:05 +0900

    ⚡ :: usePeriod 로직 변경

--- src/hooks/usePeriod.ts
+++ src/hooks/usePeriod.ts
@@ -3,18 +3,18 @@ import { useState } from "react";
 export const usePeriod = () => {
   const getPeriod = () => {
     const date = new Date();
-    const month = date.getMonth().toString();
-    const day = date.getDay().toString();
+    const month = (date.getMonth() + 1).toString();
+    const day = date.getDate().toString();
     const year = date.getFullYear().toString();
-    const today = year + "/" + month.padStart(2, "0");
-    +"/" + day.padStart(2, "0");
+    const today =
+      year + "/" + month.padStart(2, "0") + "/" + day.padStart(2, "0");
     const dateA = new Date(today + " 17:30:00");
     const diffMSec = date.getTime() - dateA.getTime();
     const diffMin = diffMSec * (1 / (60 * 1000));
-
-    if (diffMin <= 130) {
+    console.log("diffmin", diffMin);
+    if (diffMin < 120 && diffMSec >= 0) {
       return 9;
-    } else if (diffMin > 130) {
+    } else if (diffMin >= 120) {
       return 10;
     } else {
       return 8;
