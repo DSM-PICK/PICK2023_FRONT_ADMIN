84322960f35b96b5783d53cd75e0e8398bd87a88
inung1004 <dksdbswl10430@naver.com>
2023-05-25 10:24:14 +0900

    ⚡️ :: (#119) 인터페이스 추가

--- src/models/common/index.ts
+++ src/models/common/index.ts
@@ -2,3 +2,10 @@ export interface ItemType {
   option: string;
   id: string | number;
 }
+
+export interface weekendMealStudentType {
+  userId: string;
+  number: number;
+  name: string;
+  status: string;
+}
