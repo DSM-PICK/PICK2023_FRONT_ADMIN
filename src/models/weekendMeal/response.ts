612bb335fdad307ab20a18afffef6b30c25e3d01
inung1004 <dksdbswl10430@naver.com>
2023-05-31 20:23:08 +0900

    ⚡️ :: (#121) 사용 안 하는 인터페이스 삭제

--- src/models/weekendMeal/response.ts
+++
@@ -1,6 +0,0 @@
-import { weekendMealStudentType } from "../common";
-
-export interface WeekendMealStudentListResponseDto {
-  response_students: weekendMealStudentType[];
-  non_response_students: weekendMealStudentType[];
-}
