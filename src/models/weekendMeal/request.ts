0ecee1e043affddfefc5818db899cc9e5ef39de9
inung1004 <dksdbswl10430@naver.com>
2023-06-16 16:12:51 +0900

    ⚡️ :: 담임선생님 확인 DTO 추가

--- src/models/weekendMeal/request.ts
+++ src/models/weekendMeal/request.ts
@@ -2,3 +2,9 @@ export interface WeekendMealStudentListRequestDto {
   gradeNum: number;
   classNum: number;
 }
+
+export interface CheckTeacherRequestDto {
+  gradeNum: number;
+  classNum: number;
+  isCheck: boolean;
+}
