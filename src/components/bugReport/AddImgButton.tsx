aad09912ca2e46dd4c642db96423072d1e89d4d7
inung1004 <dksdbswl10430@naver.com>
2023-10-12 23:00:55 +0900

    feat : : AddImgButton 컴포넌트 추가

--- src/components/bugReport/AddImgButton.tsx
+++ src/components/bugReport/AddImgButton.tsx
@@ -1,13 +1,76 @@
 import { AddImgIcon } from "@/assets/bug-report";
+import { FileInfoType } from "@/models/common";
 import styled from "@emotion/styled";
 import Image from "next/image";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
 
-const AddImgButton = ({ onClick }: { onClick: () => void }) => {
+interface AddImgButtonProps {
+  setFileInfo: React.Dispatch<React.SetStateAction<FileInfoType | undefined>>;
+  fileInfo: FileInfoType;
+}
+
+const AddImgButton = ({ fileInfo, setFileInfo }: AddImgButtonProps) => {
+  const fileInput = useRef<HTMLInputElement>(null);
+  const [previewImg, setPreviewImg] = useState<string>("");
+  const [upLoadFile, setUpLoadFile] = useState<File>();
+
+  const onClickHandler = () => {
+    fileInput.current!.click();
+  };
+
+  const imgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file: File | null = e.target.files![0];
+
+    if (file) {
+      if (file.size > 10485760) {
+        toast.error("파일 사이즈가 10MB를 넘으면 안 됩니다.", {
+          duration: 1000,
+        });
+        return;
+      }
+      setUpLoadFile(file);
+      const reader: FileReader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          const dataUrl = reader.result as string;
+          setPreviewImg(dataUrl);
+
+          setFileInfo({
+            original_filename: file.name,
+            content_type: file.type,
+            file_size: file.size,
+            file: file,
+          });
+        }
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      setUpLoadFile(upLoadFile);
+      setPreviewImg(previewImg);
+    }
+  };
+
   return (
-    <Container onClick={onClick}>
-      <Image src={AddImgIcon} alt="이미지 추가 버튼 " />
-      <p>사진을 등록해주세요.</p>
-    </Container>
+    <div>
+      {previewImg ? (
+        <SelectImg onClick={onClickHandler} src={previewImg as string} />
+      ) : (
+        <Container onClick={onClickHandler}>
+          <Image src={AddImgIcon} alt="이미지 추가 버튼 " />
+          <p>사진을 등록해주세요.</p>
+        </Container>
+      )}
+      <input
+        onChange={imgChange}
+        type="file"
+        accept="image/png"
+        ref={fileInput}
+        style={{ display: "none" }}
+      />
+    </div>
   );
 };
 
@@ -27,6 +90,11 @@ const Container = styled.button`
     line-height: 24px;
     font-weight: 400;
   }
+`;
+
+const SelectImg = styled.img`
+  height: 230px;
+  border-radius: 12px;
 `;
 
 export default AddImgButton;
